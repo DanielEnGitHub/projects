5423105651176a8d295f38536430799c57dad399
Danie Gallina <danieljgallina@gmail.com>
2021-04-05 12:43:25 -0600

    Registro de usuarios (usuarios)

--- proyecto5/apps/adopcion/urls.py
+++ proyecto5/apps/adopcion/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index),
     path('solicitud/solicitudVer', views.SolicitudList.as_view(), name="solicitudVer"),
     path('solicitud/nuevo', views.SolicitudCreate.as_view(), name="solicitud_crear"),
+    path('solicitud/solicitudEdit/<pk>', views.SolicitudUpdate.as_view(), name="solicitudEdit"),
+    path('solicitud/solicitudDelete/<pk>', views.SolicitudDelete.as_view(), name="solicitudDelete"),
 ]
